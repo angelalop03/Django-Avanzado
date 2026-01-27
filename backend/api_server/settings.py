26ba020395d538918970b72234c66e0b9babbf06
Angela <anglopoli1@alum.us.es>
2026-01-27 18:35:33 +0100

    feat: Update database settings to use MySQL and add initial migration files for Adopter, Animal, and AdoptionRequest models

--- backend/api_server/settings.py
+++ backend/api_server/settings.py
@@ -76,11 +76,17 @@ WSGI_APPLICATION = 'api_server.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'refugio_db',
+    'USER': 'django_user',
+    'PASSWORD': 'password123',
+    'HOST': 'localhost',
+    'PORT': '3306',
+  }
 }
+
+
 
 
 # Password validation
