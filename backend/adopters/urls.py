eb3db6bbbefc9dc83700dc55047fa19b2a7062e1
Angela <anglopoli1@alum.us.es>
2026-01-27 19:00:49 +0100

    feat: Add URL routing and viewset for Adopter management

---
+++ backend/adopters/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import AdopterViewSet
+
+router = DefaultRouter()
+router.register('adopters', AdopterViewSet, basename='adopters')
+
+urlpatterns = [
+    path('adopters/', include(router.urls)),
+]
--- backend/adopters/views.py
+++ backend/adopters/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Adopter
+from serializer import AdopterSerializer
 
 # Create your views here.
+# ======== #
+# ViewSets #
+# ======== #
+
+#(GET, POST, PUT, DELETE) /adopters/
+class AdopterViewSet(ModelViewSet):
+    queryset = Adopter.objects.all().order_by('-created_at')
+    serializer_class = AdopterSerializer
+    lookup_field = 'pk'
