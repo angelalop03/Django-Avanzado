c6929150a292bde74fd9e08e5484bb430b121c85
Angela <anglopoli1@alum.us.es>
2026-01-27 18:57:37 +0100

    feat: Implement views and URL patterns for managing AdoptionRequest entities

---
+++ backend/adoptions/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import (
+    AdoptionListView,
+    AdoptionCreateView,
+    AdoptionDetailView,
+    AdoptionUpdateView,
+    AdoptionDeleteView,
+)
+
+urlpatterns = [
+    path('adoptions/', AdoptionListView.as_view()),
+    path('adoptions/create/', AdoptionCreateView.as_view()),
+    path('adoptions/<int:pk>/', AdoptionDetailView.as_view()),
+    path('adoptions/<int:pk>/update/', AdoptionUpdateView.as_view()),
+    path('adoptions/<int:pk>/delete/', AdoptionDeleteView.as_view()),
+]
--- backend/adoptions/views.py
+++ backend/adoptions/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import AdoptionRequest
+from serializer import AdoptionSerializer
 
 # Create your views here.
+
+#(GET) List all adoption requests
+class AdoptionListView(generics.ListAPIView):
+    queryset = AdoptionRequest.objects.all()
+    serializer_class = AdoptionSerializer
+
+#(POST) Create a new adoption request
+class AdoptionCreateView(generics.CreateAPIView):
+    queryset = AdoptionRequest.objects.all()
+    serializer_class = AdoptionSerializer
+
+#(GET) Retrieve a specific adoption request by ID
+class AdoptionDetailView(generics.RetrieveAPIView):
+    queryset = AdoptionRequest.objects.all()
+    serializer_class = AdoptionSerializer
+
+#(PUT) Update a specific adoption request by ID
+class AdoptionUpdateView(generics.UpdateAPIView):
+    queryset = AdoptionRequest.objects.all()
+    serializer_class = AdoptionSerializer
+
+#(DELETE) Delete a specific adoption request by ID
+class AdoptionDeleteView(generics.DestroyAPIView):
+    queryset = AdoptionRequest.objects.all()
+    serializer_class = AdoptionSerializer
+
