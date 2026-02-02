215ac993cbc1abee484705ab33066cde6dc44e9c
Angela <anglopoli1@alum.us.es>
2026-02-02 15:29:00 +0100

    feat: Add generic views for listing, creating, updating, and deleting adopters

--- backend/adopters/views.py
+++ backend/adopters/views.py
@@ -1,6 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import Adopter
 from adopters.serializer import AdopterSerializer
+from rest_framework import generics
 
 # Create your views here.
 # ======== #
@@ -12,3 +13,30 @@ class AdopterViewSet(ModelViewSet):
     queryset = Adopter.objects.all().order_by('-created_at')
     serializer_class = AdopterSerializer
     lookup_field = 'pk'
+
+# ========= #
+# Generics  #
+# ========= #
+
+#(GET) Listar todos los adoptantes
+class AdopterListView(generics.ListAPIView):
+    queryset = Adopter.objects.all()
+    serializer_class = AdopterSerializer
+
+#(POST) Crear un nuevo adoptante
+class AdopterCreateView(generics.CreateAPIView):
+    queryset = Adopter.objects.all()
+    serializer_class = AdopterSerializer
+
+
+#(PUT) Actualizar un adoptante por su ID
+class AdopterUpdateView(generics.UpdateAPIView):
+    queryset = Adopter.objects.all()
+    serializer_class = AdopterSerializer
+
+#(DELETE) Eliminar un adoptante por su ID
+class AdopterDeleteView(generics.DestroyAPIView):
+    queryset = Adopter.objects.all()
+    serializer_class = AdopterSerializer
+
+
